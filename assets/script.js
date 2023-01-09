040719549ad20bb3bc5d4a30308fa55b9e1fbe7c
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-01-09 15:49:05 -0800

    fixed character amount issue

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,33 @@ const generatePassword = function() {
     var upperCase = window.confirm("Would you like to use uppercase letters?");
     var lowerCase = window.confirm("Would you like to use lowercase letters?");
     var num = window.confirm("Would you like to use numbers?");
-  
+
+    if (specialChar) {
+        pool = pool.concat(specialCharacters);
+      }
+      if (upperCase) {
+        pool = pool.concat(alphabet);
+      }
+      if (lowerCase) {
+        pool = pool.concat(alphabetLower);
+      }
+      if (num) {
+        pool = pool.concat(numbers);
+      }
+      // make a thing that doesn't allow them to not select any type of character
+      if (characterAmount >= 8 && characterAmount <= 128) {
+        var poolPassword = ""
+        for (i = 0; i < characterAmount; i++) {
+          var index = Math.floor(Math.random() * pool.length)
+          poolPassword = poolPassword + pool[index]
+        }
+      }
+      if (!specialChar && !upperCase && !lowerCase && !num) {
+        window.alert("Come on man... how do you expect me to generate a password with no letters, numbers or special characters?? Please select one");
+        return("just pick one it's not that hard");
+      }
+      return poolPassword
+    }
  
 // Write password to the #password input
 function writePassword() {
